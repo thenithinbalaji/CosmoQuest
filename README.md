fb12cf8ceb5ec2019c4f551296d9d110648f75d5
thenithinbalaji <thenithinbalaji@gmail.com>
2024-03-02 22:50:44 +0530

    init project

---
+++ README.md
@@ -0,0 +1,27 @@
+# Getting Started with CosmoQuest
+
+<p align="center">
+<a href="#">
+    <img src="https://skillicons.dev/icons?i=html,tailwind,flask" />
+</a>
+</p>
+
+## Running the Server
+
+In the project directory, you can run:
+
+```bash
+python app.py
+```
+
+Open <http://localhost:5000> to view it in your browser.
+
+## Missing Modules
+
+Recommended to create virtual environment and install modules from requirements.txt using:
+
+```bash
+python -m virtualenv venv
+venv/scripts/activate
+pip install -r requirements.txt
+```
---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "vaishnave nithin"
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/earth")
+def earth():
+    return render_template("earth.html")
+
+@app.route("/mars")
+def mars():
+    return render_template("mars.html")
+
+@app.route("/moon")
+def moon():
+    return render_template("moon.html")
+
+@app.route("/jupiter")
+def jupiter():
+    return render_template("jupiter.html")
+
+@app.route("/saturn")
+def saturn():
+    return render_template("saturn.html")
+
+if __name__ == "__main__":
+	app.run(debug=True)
